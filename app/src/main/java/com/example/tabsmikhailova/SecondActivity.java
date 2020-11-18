b3aea555c494908e02743b8167e0d672ccc86e16
Mikhykt <mikhykt@gmail.com>
2020-11-19 00:25:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tabsmikhailova/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.tabsmikhailova;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SecondActivity extends Fragment
+
+{
+//Cоздание функии View
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.second_main, container, false);
+    }
+
+}
